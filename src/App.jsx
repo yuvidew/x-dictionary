d94581fa6634123815f873f6c9fd39d61cf26802
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-06-16 01:30:10 +0530

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react'
-import { Card, CardContent, CardTitle } from './components/ui/card'
-import { Input } from './components/ui/input'
-import { Button } from './components/ui/button'
+import { useState } from 'react'
 
-const App = () => {
-
+function App() {
   const [dictionary] = useState([
     { word: "React", meaning: "A JavaScript library for building user interfaces." },
     { word: "Component", meaning: "A reusable building block in React." },
@@ -26,37 +22,27 @@ const App = () => {
   else {
     setWordMeaning("Word not found in the dictionary.");
   }  
-}
+}   
 
   return (
-    <div>
-      <Card className = "m-auto w-[30rem] mt-[3rem] bg-stone-800 border-none p-3 shadow-lg">
-        <div className=' flex items-center gap-3'>
-          <Input 
-            type="text"  
-            value={word}
-            onChange={(e) => setWord(e.target.value)}
-            placeholder = "Search for a word..."
-            className = "border border-stone-700 placeholder:text-stone-100 text-stone-100"  
-          />
-          <Button variant = "ghost" onClick={handleSearch}>
-            Search
-          </Button>
+    <>  
+      <div>
+        <h2>Dictionary App</h2>
+        <div>
+            <input 
+              type="text"  
+              value={word}
+              onChange={(e) => setWord(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
         </div>
-      </Card>
-      <Card className = "m-auto w-[30rem] mt-[3rem] bg-stone-800 border-none p-3 shadow-lg">
-        <CardContent className = "p-2">
-          <CardTitle className = "text-[1rem] text-stone-100">
-            Definition :
-          </CardTitle>
-          {wordMeaning && 
-            <p className=' text-stone-300 mt-4'>{wordMeaning}</p>
-          }
-        </CardContent>
-      </Card>
-    </div>
+        <div>
+          <h2>Definition:</h2>
+          <p>{wordMeaning}</p>
+        </div> 
+      </div>
+    </>
   )
 }
 
 export default App
-
